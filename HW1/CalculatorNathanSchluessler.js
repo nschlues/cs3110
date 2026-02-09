5d42e96f625293e3e61fa5aaf06e77cfe308c85b
nschlues <nschlues@uccs.edu>
2026-02-09 07:11:04 -0700

    Finished HW2
    Satisfied all of the hw criteria including adding/removing a DOM element as a result of interacting with the calculator

--- HW1/CalculatorNathanSchluessler.js
+++ HW1/CalculatorNathanSchluessler.js
@@ -1,15 +1,22 @@
-document.body.addEventListener('click', () => 
+document.getElementById('type').addEventListener('click', () => 
     document.getElementById('solution'). innerText = "Typing...")
 
 // This stop propagation is needed because otherwise, the different click effect of the 
-// unique key would not be resolved. It would still say typing... instead of "Duh"
+// equals key would not be resolved. It would still say "typing..." instead of providing the 
+// helpful solution
 document.getElementById("equals").addEventListener('click', (e) => e.stopPropagation())
 
 document.getElementById("equals").addEventListener('click', () =>
-    document.getElementById("solution"). innerText = "😂 Duh 😂")
+    document.getElementById("solution"). innerText = "Duh")
+
+document.getElementById('equals').addEventListener('click', () =>
+    document.getElementById('support').innerText = "😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂")
 
 // Same reasoning as above
 document.getElementById("reset").addEventListener('click', (e) => e.stopPropagation())
 
 document.getElementById("reset").addEventListener('click', () =>
     document.getElementById("solution"). innerText = "Please enter a value")
+
+document.getElementById('reset').addEventListener('click', () =>
+    document.getElementById('support').innerText = "")
